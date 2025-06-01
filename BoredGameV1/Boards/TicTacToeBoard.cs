c25c14167dab06f702699caf91703f3c4559e719
entropyinversed <tyler.r.mumford@gmail.com>
2025-06-01 11:21:40 +0200

    Made TicTacToeBoard more readable.

--- BoredGameV1/Boards/TicTacToeBoard.cs
+++ BoredGameV1/Boards/TicTacToeBoard.cs
@@ -2,15 +2,20 @@ namespace BoredGame.Boards;
 
 public class TicTacToeBoard : IBoard
 {
-    private readonly char[,] _cells = new char[3, 3];
+    private const byte SquareBoardLength = 3;
     
+    private readonly char[,] _cells = new char[SquareBoardLength, SquareBoardLength];
     public char[,] Cells => _cells;
 
     public void Setup()
     {
-        for (var row = 0; row < 3; row++)
-            for (var col = 0; col < 3; col++)
+        for (var row = 0; row < SquareBoardLength; row++)
+        {
+            for (var col = 0; col < SquareBoardLength; col++)
+            {
                 _cells[row, col] = '_';
+            } 
+        }
     }
 
     public void Display()
@@ -19,9 +24,9 @@ public class TicTacToeBoard : IBoard
         Console.WriteLine("=========");
         Console.WriteLine();
 
-        for (var row = 0; row < 3; row++)
+        for (var row = 0; row < SquareBoardLength; row++)
         {
-            for (var col = 0; col < 3; col++)
+            for (var col = 0; col < SquareBoardLength; col++)
             {
                 Console.Write($" {_cells[row, col]} ");
             }
@@ -34,6 +39,8 @@ public class TicTacToeBoard : IBoard
 
     public bool TryPlaceMark(int row, int col, char mark)
     {
+        // TODO: Make more readable
+        
         if (row is < 0 or > 2 || col is < 0 or > 2) return false;
         if (_cells[row, col] != '_') return false;
         _cells[row, col] = mark;
