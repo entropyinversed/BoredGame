ed2888983cec085db30f63f90e51df30939a5dca
entropyinversed <tyler.r.mumford@gmail.com>
2025-10-12 20:49:00 +0200

    So that's a logger...

--- BoredGame/Program.cs
+++ BoredGame/Program.cs
@@ -1,11 +1,23 @@
-﻿namespace BoredGame;
+﻿using Microsoft.Extensions.Logging;
+
+namespace BoredGame;
 
 internal static class Program
 {
     private static void Main()
     {
+        using ILoggerFactory factory = LoggerFactory.Create(builder => builder.AddConsole());
+        ILogger logger = factory.CreateLogger("Program");
+        logger.LogInformation("Just a logging test!");
+
+        Console.WriteLine("Quick Test");
         Console.WriteLine("---Starting Board Game---");
 	
+        //TestClass(logger).LogAgain();
+
+        TestClass testClass = new TestClass(logger);
+        testClass.LogAgain();
+/*
 	    var gameType = InputManager.PromptForGameType();
         var game = GameFactory.CreateGame(gameType);
         game.Start();
@@ -17,5 +29,15 @@ internal static class Program
 	        game.PlayTurn();
             game.DisplayBoard();
         }
+*/
+    }
+}
+
+public class TestClass(ILogger logger)
+{
+    public void LogAgain()
+    {
+        Console.WriteLine("here we are bro");
+        logger.LogInformation("Im in here now");
     }
 }
